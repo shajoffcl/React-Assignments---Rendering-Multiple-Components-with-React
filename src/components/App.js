fd7eab29de2276d22b24e71b956dc046fc947dc6
Mohd Shaib <shaj.native@gmail.com>
2020-11-09 14:23:26 +0530

    changes done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-ns-test="project-name">Job Portal Website</h1>
+      <p data-ns-test="project-description">
+        A website for job seeker who belongs to non-skills area and for daily
+        workers.
+      </p>
+    </>
+  );
+};
 
 export default App;
